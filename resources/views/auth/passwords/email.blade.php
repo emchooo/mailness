ce7c397efc7d73c7eb87566eaf637a1cb74c906d
emchooo <emir.softic@gmail.com>
2019-09-23 22:23:22 +0200

    reset password page style

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,56 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Mailness</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+</head>
+<body class="font-openSans bg-gray-200">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="mb-3 mt-10 text-gray-600 text-center text-2xl">
+    Mailness
 </div>
-@endsection
+
+<div class="text-gray-600 text-l text-center">
+    Reset password
+</div>
+
+<div class="flex justify-center mt-10" >
+<div class="w-full max-w-xs center">
+
+  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="POST" action="{{ route('password.email') }}" >
+  @csrf
+
+  @if (session('status'))
+        <div class="bg-green-100 border-l-4 border-green-500 text-green-700 p-2 my-2">
+            {{ session('status') }}
+        </div>
+    @endif
+
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+        Email
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="email" value="{{ old('email') }}" placeholder="Email address">
+      @error('email')
+        <p class="text-red-500 text-xs italic pt-2">{{ $message }}</p>
+      @enderror
+    </div>
+
+
+    <div class="">
+      <button class="bg-blue-500 hover:bg-blue-700 w-full text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+        {{ __('Send Password Reset Link') }}
+      </button>
+    </div>
+  </form>
+</div>
+</div>
+
+</body>
+</html>
